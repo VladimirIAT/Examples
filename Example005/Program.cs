72c480644837f6ba374a490e6797bff74da58591
Vladimir <volodko127@mail.ru>
2022-12-14 23:24:02 +0300

    Added Example005

---
+++ Example005/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите имя пользователя");
+string username = Console.ReadLine();
+if(username == "Маша")
+{
+    Console.WriteLine("Ура, это Маша!");
+} else {
+    Console.WriteLine("Привет, " + username);
+}
